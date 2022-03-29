fcdd92d823fa5e558858b998429ca7bc34772a74
Forestiak <marusarzdawid@gmail.com>
2022-03-29 22:55:09 +0200

    cocktails

---
+++ cocktails.cpp
@@ -0,0 +1,28 @@
+class Cocktails {
+    public:
+
+        void ginTonic(){
+
+            #define GIN_WEIGHT  //add weight
+            #define TONIC_WEIGHT //add weight
+
+            static ginPump {PUMP_GIN_PIN};
+            static tonicPump {PUMP_TONIC_PUMP};
+            unsigned long long currentTime;
+            unsigned long long previousTime;
+
+            glassWarning();
+
+            
+
+
+
+
+
+
+    }
+
+
+
+
+}
