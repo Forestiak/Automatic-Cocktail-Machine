b9299b649485a7439b1b87aa17148463304e7f9e
Forestiak <93220998+Forestiak@users.noreply.github.com>
2022-04-12 14:36:49 +0200

    Update function-test.cpp

--- function-test.cpp
+++ function-test.cpp
@@ -54,7 +54,7 @@ int main(int argc, char **argv){
 
     Cocktails cocktails;
     int hh;
-    //hh = lgGpiochipOpen(0);
+    hh = lgGpiochipOpen(0);
 
     while(1)    
         cocktails.ginTonic();
@@ -63,8 +63,8 @@ int main(int argc, char **argv){
 
 int ginTonic(){
 
-    #define GIN_WEIGHT 200                              //add weight
-    #define TONIC_WEIGHT 100                            //add weight
+    const int GIN_WEIGHT = 200;                              //add weight
+    const int TONIC_WEIGHT = 100;                            //add weight
 
     const int ginPump {PUMP_GIN_PIN};
     const int tonicPump {PUMP_TONIC_PIN};
@@ -72,29 +72,29 @@ int ginTonic(){
     double currentWeight;
 
     int hh;
-    //hh = lgGpiochipOpen(0);                             //activates the GPIO pins
-    //lgGpioClaimOutput(hh, 0, ginPump, 1);               //sets the ginPump as an output
+    hh = lgGpiochipOpen(0);                             //activates the GPIO pins
+    lgGpioClaimOutput(hh, 0, ginPump, 1);               //sets the ginPump as an output
 
     void zero(Options o = Options());                   //zeros the scale
-    //lgGpioWrite(hh, ginPump, 1);                        //turns on the ginPump
+    lgGpioWrite(hh, ginPump, 1);                        //turns on the ginPump
 
     for(;;){
         currentWeight = checkWeight();                  //constantly checks the weight on the load cell
 
         if (currentWeight >= GIN_WEIGHT){               //if the weight on the load cell is equal or higher than the defined value,
-            //lgGpioWrite(hh, ginPump, 0);                //then it stops the peristaltic pump
+            lgGpioWrite(hh, ginPump, 0);                //then it stops the peristaltic pump
             break;
         }    
     }
 
     void zero (Options o = Options());                  //zeros the scale
-    //lgGpioWrite(hh, tonicPump, 1);                      //turns on the tonicPump
+    lgGpioWrite(hh, tonicPump, 1);                      //turns on the tonicPump
 
     for(;;){
         currentWeight = checkWeight();                  //constantly checks the weight on the load cell
 
         if (currentWeight >= TONIC_WEIGHT){             //if the weight on the load cell is equal or higher than the defined value,
-            //lgGpioWrite(hh, tonicPump, 0);              //then it stops the peristaltic pump
+            lgGpioWrite(hh, tonicPump, 0);              //then it stops the peristaltic pump
             break;
         }
     }
