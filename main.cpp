7dfd43a3ef2f9e609e462691feec402f86252fc8
Forestiak <93220998+Forestiak@users.noreply.github.com>
2022-04-24 21:00:03 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,15 +6,19 @@
 #include <hx711/common.h>
 #include <lgpio.h>
 
-#define PUMP_GIN_PIN 2            //add specific pins to all of them
-#define PUMP_TONIC_PIN
+
+#define PUMP_GIN_PIN 2
+#define PUMP_TONIC_PIN 3
 #define PUMP_MANGO_PIN
 #define PUMP_VODKA_PIN
 #define PUMP_ORANGE_PIN
 #define PUMP_COFFEE_PIN
 #define PUMP_LEMONADE_PIN
-#define LOAD_CELL_DATA_PIN 17
-#define LOAD_CELL_CLOCK_PIN 27
+
+#define dataPin 17
+#define clockPin 27
+#define refUnit 234
+#define offset -148699
 
 using namespace std;
 using namespace HX711;
@@ -22,68 +26,113 @@ using namespace std::this_thread;
 using namespace std::chrono;
 
 
-int checkWeight() {
 
-    const int dataPin = 17;
-    const int clockPin = 27;
-    const int refUnit = 40241;
-    const int offset = -3670017;
+double setWeight(){
+    AdvancedHX711 hx(dataPin, clockPin, refUnit, offset);
+    double previousWeight = hx.weight(15);
 
-    SimpleHX711 hx(dataPin, clockPin, refUnit, offset);
-
-    while(1){
-
-        const Mass m = hx.weight(3);
-
-        cout    << "\t" << m.getValue() << '\n'
-                << "\t" << m.toString(Mass::Unit::G) << '\n'
-                << endl;
-
-        if(m.getValue()>70)
-            break;
-        
-        sleep_for(seconds(1));      
-    }
-    return EXIT_SUCCESS;
+    return previousWeight;
 }
+
+double checkWeight() {
+    AdvancedHX711 hx(dataPin, clockPin, refUnit, offset);
+    double weight = hx.weight(milliseconds(150));
+    //cout << currentWeight << endl;
+    return weight;
+}
+
 
 
 class Cocktails {
     public:
 
-        int ginTonic(){
+         int prepareDrink(const int firstWeight, const int secondWeight, const int firstPumpPin, const int secondPumpPin){ 
 
-            #define GIN_WEIGHT  //add weight
-            #define TONIC_WEIGHT //add weight
-
-            const int ginPump {PUMP_GIN_PIN};
-            const int tonicPump {PUMP_TONIC_PIN};
-
+            double weight = 0;
+            double previousWeight = 0;
             int hh;
-            hh = lgGpiochipOpen(0);
 
-            lgGpioClaimOutput(hh, 0, ginPump, 1);
-            lgGpioWrite(hh, ginPump, 1);
+            //claim the pumps as outputs
+            hh = lgGpiochipOpen(0);                             
+            lgGpioClaimOutput(hh, 0, firstPumpPin, 1); 
+            lgGpioClaimOutput(hh, 0, secondPumpPin, 1);      
 
-            checkWeight();
-            lgGpioWrite(hh, ginPump, 0);
+            //tare the scale with a glass on it
+            cout << "Put the glass on the scale" << endl;
+            cin.ignore();
+            cout << "Calibrating..." << endl;
+            previousWeight = setWeight();
+            sleep_for(seconds(2));
 
+            //start the gin pump
+            lgGpioWrite(hh, firstPumpPin, 1);
+
+            //start the loop in which we are checking for the current weight on the load cell
+            for(;;){
+                weight = checkWeight(); 
+                weight -= previousWeight;                     
+                cout << weight << endl;
+
+            //when the weight is >= as we declared, then it stops the pumps
+                if (weight >= firstWeight){               
+                    lgGpioWrite(hh, firstPumpPin, 0);              
+                    break;
+                }    
+            }
+            sleep_for(seconds(2));
+            previousWeight = setWeight();
+            cout << "Calibrating..." << endl;
+            sleep_for(seconds(2));
+
+            lgGpioWrite(hh, secondPumpPin, 1);                     
+
+            for(;;){
+                weight = checkWeight();
+                weight -= previousWeight;   
+
+                cout << weight << endl;              
+
+                if (weight >= secondWeight){             
+                    lgGpioWrite(hh, secondPumpPin, 0);    
+                    break;
+                }
+            }
+            cout << "FINISHED" << endl;
             return 0;
-        }      
-};
+        }
 
-int glassWarning(){   
-    std::cout << "Put the glass under the nozzle and click continue" << std::endl;
-    return 0;
+        int ginTonic(const int firstWeight, const int secondWeight, const int firstPumpPin, const int secondPumpPin)
+            {prepareDrink(firstWeight, secondWeight, firstPumpPin, secondPumpPin);};
+        
+        int ginHass(const int firstWeight, const int secondWeight, const int firstPumpPin, const int secondPumpPin)
+            {prepareDrink(firstWeight, secondWeight, firstPumpPin, secondPumpPin);};
+
+        int screwdriver(const int firstWeight, const int secondWeight, const int firstPumpPin, const int secondPumpPin)
+            {prepareDrink(firstWeight, secondWeight, firstPumpPin, secondPumpPin);};
+
+        int blackRussian(const int firstWeight, const int secondWeight, const int firstPumpPin, const int secondPumpPin)
+            {prepareDrink(firstWeight, secondWeight, firstPumpPin, secondPumpPin);};
+
+        int summerCollins(const int firstWeight, const int secondWeight, const int firstPumpPin, const int secondPumpPin)
+            {prepareDrink(firstWeight, secondWeight, firstPumpPin, secondPumpPin);};
+
+      
+       
 };
 
 
 int main(int argc, char **argv){
 
+    
     Cocktails cocktails;
     int hh;
     hh = lgGpiochipOpen(0);
 
-    while(1)    
-        cocktails.ginTonic();
+      
+    //cocktails.ginTonic();
+
+    for(;;)
+        cocktails.ginTonic(200, 100, PUMP_GIN_PIN, PUMP_TONIC_PIN);
+
+    return 0;
 }
