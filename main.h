96f927052aa789015a5f236014a98d0a65dbf543
Forestiak <93220998+Forestiak@users.noreply.github.com>
2022-04-27 22:27:12 +0200

    Link to the declared values in main

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+extern const int PUMP_GIN_PIN;
+extern const int PUMP_TONIC_PIN;
+extern const int PUMP_MANGO_PIN;
+extern const int PUMP_VODKA_PIN;
+extern const int PUMP_ORANGE_PIN;
+extern const int PUMP_COFFEE_PIN;
+extern const int PUMP_LEMONADE_PIN;
+extern const int PUMP_EIGHT_PIN;
+
+extern const int dataPin;
+extern const int clockPin;
+extern const int refUnit;
+extern const int offset;
+
+#endif
