5fb80f039cfeadce2c8f163ba703bdb10367e901
Forestiak <93220998+Forestiak@users.noreply.github.com>
2022-05-09 18:48:55 +0200

    Python interface

--- interface3.py
+++ interface3.py
@@ -1,7 +1,21 @@
 import tkinter as tk
+import sys
+import os
+import libCocktails
+import lgpio as sbc
+
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using :0.0')
+    os.environ.__setitem__('DISPLAY', ':0.0')
+
 
 def testf():
     print("button clicked")
+
+
+
+h = sbc.gpiochip_open(0)
+x = libCocktails.Cocktails()
 
 root = tk.Tk()
 
@@ -11,27 +25,29 @@ canvas.pack()
 # ginTonicImage = tk.PhotoImage(file='gin-and-tonic-drink.png')
 
 frame = tk.Frame(root, bg='#80c1ff', bd=5)
-# pobaw się ustawieniami wielkości ramki
+
 frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.8, anchor='n')
 
-# test guzika
-# ginTonicButton2 = tk.Button(frame, image=ginTonicImage, text='GIN TONIC', font=70)
-# ginTonicButton2.pack()
 
-ginTonicButton = tk.Button(frame, text="Gin Tonic", font=70, command=testf)
-# ginTonicButton.pack()
+
+ginTonicButton = tk.Button(frame, text="Gin Tonic", font=70, command=x.ginTonic)
 ginTonicButton.place(relx=0.05, rely=0.1, relwidth=0.25, relheight=0.25)
 
-ginHassButton = tk.Button(frame, text="Gin Hass", font=70)
+ginHassButton = tk.Button(frame, text="Gin Hass", font=70, command=x.ginHass)
 ginHassButton.place(relx=0.35, rely=0.1, relwidth=0.25, relheight=0.25)
 
-screwdriverButton = tk.Button(frame, text="Screwdriver", font=70)
+screwdriverButton = tk.Button(frame, text="Screwdriver", font=70, command=x.screwdriver)
 screwdriverButton.place(relx=0.65, rely=0.1, relwidth=0.25, relheight=0.25)
 
-blackRussianButton = tk.Button(frame, text="BlackRussian", font=70)
-blackRussianButton.place( relx=0.1, rely=0.6, relwidth=0.25, relheight=0.25)
+blackRussianButton = tk.Button(frame, text="Black Russian", font=70, command=x.blackRussian)
+blackRussianButton.place( relx=0.05, rely=0.6, relwidth=0.25, relheight=0.25)
 
-summerCollinsButton = tk.Button(frame, text="Summer Collins", font=70)
-summerCollinsButton.place( relx=0.6, rely=0.6, relwidth=0.25, relheight=0.25)
+summerCollinsButton = tk.Button(frame, text="Summer Collins", font=70, command=x.summerCollins)
+summerCollinsButton.place( relx=0.35, rely=0.6, relwidth=0.25, relheight=0.25)
+
+exitButton = tk.Button(frame, text="Exit", font=70, command=root.destroy)
+exitButton.place( relx=0.65, rely=0.6, relwidth=0.25, relheight=0.25)
+
+root.attributes('-fullscreen', True)
 
 root.mainloop()
