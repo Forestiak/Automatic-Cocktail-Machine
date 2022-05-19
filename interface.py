918e2a27c31efa58262082e2d01ddd0fac891a9b
Forestiak <93220998+Forestiak@users.noreply.github.com>
2022-05-19 09:53:22 +0200

    Update interface.py

--- interface.py
+++ interface.py
@@ -6,6 +6,8 @@ import lgpio as sbc
 import time
 
 
+#because of that it is possible to display the GUI on the raspberry's display from SSH terminal
+
 if os.environ.get('DISPLAY','') == '':
     print('no display found. Using :0.0')
     os.environ.__setitem__('DISPLAY', ':0.0')
@@ -14,12 +16,14 @@ if os.environ.get('DISPLAY','') == '':
 h = sbc.gpiochip_open(0)
 x = libCocktails.Cocktails()
 
+global root
 root = tk.Tk()
 
 canvas = tk.Canvas(root, height=500, width=1280)
 canvas.pack()
 
 
+#creates a new window and executes a function given in the parameter
 
 def warning(functionName):
 
@@ -37,11 +41,13 @@ def warning(functionName):
     label.place(relx=0.5, rely=0.5, anchor='center')
     
     global continueButton                     
-    continueButton = tk.Button(frame, text="Continue", command=lambda:[changeLabel(), functionName(), finishLabel(), newWindow.destroy()])
+    continueButton = tk.Button(frame, text="Continue", command=lambda:[changeLabel(), functionName(), finishLabel(), newWindow.destroy()])       #lambda allows to execute few function within one click of a button
     continueButton.pack(side='top')
                      
     newWindow.attributes('-fullscreen', True)
 
+
+#destroys the "Continue" button and changes the text on the label
                      
 def changeLabel():
     global continueButton
@@ -50,13 +56,22 @@ def changeLabel():
     global label
     label.configure(text="In progress...")
     label.update()
-                     
+        
+        
+#changes the text on the label                     
                      
 def finishLable():
     global label
     label.configure(text="Completed")
     label.update()
-    time.sleep(3)                     
+    time.sleep(3)      
+    
+
+#destroys the the main window 
+
+def rootDestroy():
+    global root
+    root.destroy()
 
     
     
@@ -65,6 +80,7 @@ frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.8, anchor='n')
 
 
 ginTonicButton = tk.Button(frame, text="Gin Tonic", font=70, command=lambda:warning(x.ginTonic))    
+
 ginTonicButton.place(relx=0.05, rely=0.1, relwidth=0.25, relheight=0.25)
 
 ginHassButton = tk.Button(frame, text="Gin Hass", font=70, command=lambda:warning(x.ginHass))
@@ -79,7 +95,7 @@ blackRussianButton.place( relx=0.05, rely=0.6, relwidth=0.25, relheight=0.25)
 summerCollinsButton = tk.Button(frame, text="Summer Collins", font=70, command=lambda:warning(x.summerCollins))
 summerCollinsButton.place( relx=0.35, rely=0.6, relwidth=0.25, relheight=0.25)
 
-exitButton = tk.Button(frame, text="Exit", font=70, command=root.destroy)
+exitButton = tk.Button(frame, text="Exit", font=70, command=rootDestroy)
 exitButton.place( relx=0.65, rely=0.6, relwidth=0.25, relheight=0.25)
 
 cleanButton = tk.Button(frame, text="Clean". font=70, command=x.cleanPumps)
